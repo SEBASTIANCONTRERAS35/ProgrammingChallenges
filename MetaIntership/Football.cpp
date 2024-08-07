03d6ee91a6be899c83136163cecc21eb727acb5c
BICHOTEE <emilio3547@outlook.es>
2024-08-07 14:32:58 -0600

    7 agosto

---
+++ MetaIntership/Football.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+string winner(const vector<string>& jugadores){
+    unordered_map<string,int> timesWin;
+    for (const auto& jugador:jugadores){
+        timesWin[jugador]++;
+    }
+    if (timesWin.size()>2){
+        return "-1";
+    }
+    int golesMax=0;
+    std::vector<std::pair<std::string, int>> vec(timesWin.begin(), timesWin.end());
+    if (vec[0].second>vec[1].second){
+        return vec[0].first;
+    }else{
+        return vec[1].first;
+    }
+}
+
+int main() {
+    int numGoals;
+    vector<string> jugadores;
+    cin>>numGoals;
+    for (int i=0;i<numGoals;i++){
+        string team;
+        cin>>team;
+        jugadores.push_back(team);
+    }
+    if (jugadores.size()==1){
+        cout<<jugadores[0];
+    }
+    else{
+       cout<< winner(jugadores);
+    }
+
+    return 0;
+}
